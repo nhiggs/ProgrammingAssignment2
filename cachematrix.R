1b9d07138fc38f59fe5a19df2a2947aa50e1607d
Nico <nicoreekohiggs@gmail.com>
2016-08-03 14:10:30 -0600

    Edited comments

--- cachematrix.R
+++ cachematrix.R
@@ -6,9 +6,11 @@
 ## the inverse without having to compute it again. If the inverse is not
 ## cached or already known, these functions will then compute the inverse.
 
-## makeCacheMatrix takes in a matrix and returns a list of functions
-## that can can set the matrix, get the matrix, set the inverse, or get
-## the inverse, while also caching the matrix and its inverse.
+## makeCacheMatrix takes in or creates a matrix and returns a fully formed
+## object of type makeCacheMatrix to be used by downstream R code. The 
+## object is a list of functions that can can set the matrix, get the 
+## matrix, set the inverse, or get the inverse, while also caching the 
+## matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
   mInverse <- NULL
@@ -24,9 +26,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve will check to see if the inverse of the matrix already exists,
-## otherwise it computes the inverse of the matrix itself, and then returns
-## the inverse.
+## cacheSolve will check to see if the inverse of the matrix already exists
+## and will then return it. Otherwise it computes the inverse of the matrix
+## itself, and then sets the inverse to the appropriate object variable.
 
 cacheSolve <- function(x, ...){
   mInverse <- x$getInverse
